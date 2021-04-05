4f1bf27eb3ed8504d487995050a7c72e623a8098
widelachie <me@lachlankemp.com>
2021-04-05 21:50:42 +0930

    sorry we're adding an ad

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,6 +12,12 @@ function Home({ launches }) {
       <Head>
         <title>Live@Space</title>
         <script data-ad-client="ca-pub-5373807673490757" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script type="text/javascript">
+            window._mNHandle = window._mNHandle || {};
+            window._mNHandle.queue = window._mNHandle.queue || [];
+            medianet_versionId = "3121199";
+        </script>
+        <script src="https://contextual.media.net/dmedianet.js?cid=8CUBV3K76" async="async"></script>
       </Head>
       {youtubeIsVisible &&
         <div className={youtubePlayerStyles.center_youtube_player}>
@@ -21,20 +27,25 @@ function Home({ launches }) {
           <span className={youtubePlayerStyles.close} onClick={function(){setYoutubeIsVisible(false); setYoutubeUrl("")}}>✕</span>
         </div>
       }
+
       <div className={styles.launches}>
         {launches.map((launch) => (
           <div key={launch.id} className={styles.griditem} style={{background:`linear-gradient(rgba(4, 4, 26, 0.7), rgba(4, 4, 26, 0.45)), url(${launch.backgroundImage})`, backgroundPositionY:`${launch.backgroundImagePlacement}%`, backgroundSize:"cover"}}>
+            
             <div className={styles.countdown}>
                 <span><GetLaunchCountdown time={launch.launchdate}/></span>
                 <div className={styles.launchtimewrapper}>
                   <p><span>Launch on:</span> <span><GetLaunchDate time={launch.launchdate}/></span></p>
                 </div>
             </div>
+
             <h3>{launch.title}</h3>
             <h4>{launch.description}</h4>
+
             {launch.youtubeWatchcode &&
               <button onClick={function(){ setYoutubeIsVisible(true); setYoutubeUrl(`https://www.youtube-nocookie.com/embed/${launch.youtubeWatchcode}`)}} className={styles.white, styles.white} href={`${launch.youtubeWatchcode}`} >{launch.buttonText}</button>
             }
+
             {!launch.youtubeWatchcode &&
               <div style={{margin:0, paddingBottom:"1rem", fontSize:"0.8rem", color:"var(--gru)"}}>
                 <span style={{ backgroundColor:"var(--darkblue-transparent)", padding:"3px", border:"var(--border)", cursor:"default", userSelect:"none"}}>A live video will appear here when avaliable</span>
@@ -42,10 +53,18 @@ function Home({ launches }) {
             }
           </div>
         ))}
+
+        <div className={styles.griditem} dangerouslySetInnerHTML={mediumAd()}/>
+        <div className={styles.griditem}>helo</div>
+
       </div>
     </>
   )
 }
+function mediumAd() {
+  return{__html: '<div id="532316696"><script type="text/javascript">try {window._mNHandle.queue.push(function (){window._mNDetails.loadTag("532316696", "300x250", "532316696");});}catch (error) {}</script></div>'}
+}
+
 export async function getStaticProps() {
   // Development
   //const res = await fetch('http://localhost:3000/launches.json'); var launches = await res.json();
