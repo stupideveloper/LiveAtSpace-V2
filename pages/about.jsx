c76ed837fab6cf421f59d7135e5b4ad39e187d96
widelachie <me@lachlankemp.com>
2021-04-07 15:36:37 +0930

    spelling mistake

--- pages/about.jsx
+++ pages/about.jsx
@@ -11,7 +11,7 @@ export default function About() {
 			<div className={styles.container}>
 				<h1>About.</h1>
 				<div>
-					<p style={{lineHeight:"1.45rem"}}><bold>Live@Space</bold> was and is developed to serve as a hub for all rocket future launches and other space activity. It all launches from SpaceX, NASA, Rocket Lab and all other areospace companies and orginisations. </p>
+					<p style={{lineHeight:"1.45rem"}}><bold>Live@Space</bold> was and is developed to serve as a hub for all rocket future launches and other space activity. It all launches from SpaceX, NASA, Rocket Lab and all other areospace companies and organizations. </p>
 					<p><bold>Live@Space</bold> is open source and proudly made in Australia with all of its code hosted on a github repository <a href="https://github.com/widelachie/LiveAtSpace-V2" target="_blank" style={{color:"#ae601c"}}>here</a>. <bold>Live@Space</bold> is supported by the community, <a href="https://github.com/widelachie/LiveAtSpace-V2" target="_blank"><bold >please consider contributing</bold></a>. <br /><br /> Now because we have no more text, enjoy this gif of Starship SN8 crashing. <a href="mailto:me@lachlankemp.com" style={{color:"#ae601c"}}>- Lachie</a></p>
 					<img src="/static/images/sn8.gif" alt="Starship SN8 Crash" />
 				</div>
